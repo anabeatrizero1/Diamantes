4aa8c162c2b1bcf9d5adbf1c7c439c4173cb423b
Dell <Dell@DESKTOP-JO7U36A>
2022-02-14 19:32:42 -0300

    Add project files.

---
+++ Diamantes.ConsoleApp/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Diamantes.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            Console.WriteLine("Digite um número: ");
+            numero = Convert.ToInt32(Console.ReadLine());
+            int meio = numero / 2 ;
+            int direita = meio, esquerda = meio;
+
+            for (int i = 0; i < meio ; i++)
+            {
+                for (int j = 0; j < numero; j++)
+                {
+                    if (j <= direita && j >= esquerda)
+                    {
+                        Console.Write("x");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                esquerda--;
+                direita++;
+                Console.WriteLine();
+            }
+            for (int i = meio; i < numero; i++)
+            {
+                for (int j = 0; j < numero; j++)
+                {
+                    if (j <= direita && j >= esquerda)
+                    {
+                        Console.Write("x");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                esquerda++;
+                direita--;
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
